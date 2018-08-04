740e4c4d9d0b27601b450ba21396287d54bf4013
Jose <joseenrique86@gmail.com>
2018-08-04 12:29:49 +0000

    Added basic folders from Spring Start

---
+++ src/main/java/com/chechecalderon/tweetbot/TweetbotApplication.java
@@ -0,0 +1,12 @@
+package com.chechecalderon.tweetbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TweetbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TweetbotApplication.class, args);
+	}
+}
