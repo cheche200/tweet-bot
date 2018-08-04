f255b56fd48ec86faab1fa82b5532a3702d7b489
Jose <joseenrique86@gmail.com>
2018-08-04 13:22:18 +0000

    Done refactor on BotChatService

--- src/main/java/com/chechecalderon/tweetbot/service/BotChatService.java
+++ src/main/java/com/chechecalderon/tweetbot/service/BotChatService.java
@@ -11,6 +11,8 @@ public class BotChatService {
 
     private static final boolean TRACE_MODE = false;
     static String botName = "super";
+    String resourcesPath = getResourcesPath();
+    Bot bot = new Bot("super", resourcesPath);
 
     public String getResponse(String request) {
 
@@ -20,43 +22,51 @@ public class BotChatService {
     private String getBotResponse(String humanRequest) {
         String response = "";
         try {
-            String resourcesPath = getResourcesPath();
+
             System.out.println(resourcesPath);
             MagicBooleans.trace_mode = TRACE_MODE;
-            Bot bot = new Bot("super", resourcesPath);
+
             Chat chatSession = new Chat(bot);
             bot.brain.nodeStats();
-            String textLine = "";
-
-
-                System.out.print("Human : ");
-                textLine = humanRequest;
-                if ((textLine == null) || (textLine.length() < 1))
-                    textLine = MagicStrings.null_input;
-                if (textLine.equals("q")) {
-                    System.exit(0);
-                } else if (textLine.equals("wq")) {
-                    bot.writeQuit();
-                    System.exit(0);
-                } else {
-                    String request = textLine;
-                    if (MagicBooleans.trace_mode)
-                        System.out.println(
-                                "STATE=" + request + ":THAT=" + ((History) chatSession.thatHistory.get(0)).get(0)
-                                        + ":TOPIC=" + chatSession.predicates.get("topic"));
-                    response = chatSession.multisentenceRespond(request);
-                    while (response.contains("&lt;"))
-                        response = response.replace("&lt;", "<");
-                    while (response.contains("&gt;"))
-                        response = response.replace("&gt;", ">");
-                    System.out.println("Robot : " + response);
-                }
+            String textLine = "Sorry I don't understand";
+            System.out.print("Human : " + textLine);
+            textLine = humanRequest;
             
+            if (isRequestLegit(textLine)) {
+                String request = textLine;
+                if (MagicBooleans.trace_mode)
+                    System.out.println(
+                            "STATE=" + request + ":THAT=" + ((History) chatSession.thatHistory.get(0)).get(0)
+                                    + ":TOPIC=" + chatSession.predicates.get("topic"));
+                response = chatSession.multisentenceRespond(request);
+                while (response.contains("&lt;"))
+                    response = response.replace("&lt;", "<");
+                while (response.contains("&gt;"))
+                    response = response.replace("&gt;", ">");
+                System.out.println("Robot : " + response);
+            }
+
         } catch (Exception e) {
             e.printStackTrace();
         }
 
         return response;
+    }
+
+    private boolean isRequestLegit(String textLine){
+        if ((textLine == null) || (textLine.length() < 1)) {
+            textLine = MagicStrings.null_input;
+            return false;
+        }
+        if (textLine.equals("q")) {
+            System.exit(0);
+            return false;
+        } else if (textLine.equals("wq")) {
+            bot.writeQuit();
+            System.exit(0);
+            return false;
+        }
+        return true;
     }
 
     private static String getResourcesPath() {
@@ -67,5 +77,7 @@ public class BotChatService {
         String resourcesPath = path + File.separator + "src" + File.separator + "main" + File.separator + "resources";
         return resourcesPath;
     }
+
+
 
 }
