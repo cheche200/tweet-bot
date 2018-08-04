4cec02752ffae6562fd10c42a338511d7cb1b180
Jose <joseenrique86@gmail.com>
2018-08-04 12:54:07 +0000

    Added Chat Bot Controller and Service

---
+++ src/main/java/com/chechecalderon/tweetbot/service/BotChatService.java
@@ -0,0 +1,11 @@
+package com.chechecalderon.tweetbot.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BotChatService {
+    public String getResponse(String request) {
+
+        return "Hello";
+    }
+}
